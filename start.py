80b53dc3e8f6c8cba77da3b2f2019c7315f286e9
P A M O D <59545375+pamodmadubashana@users.noreply.github.com>
2024-10-01 20:48:01 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,24 +1,3 @@
-# import os
-# from flask import Flask
-# import threading
-
-# app = Flask(__name__)
-
-# @app.route('/health', methods=['GET'])
-# def health_check():
-#     return 'OK', 200
-
-# def run_health_check():
-#     app.run(host='0.0.0.0', port=8443)
-
-# if __name__ == "__main__":
-
-#     health_thread = threading.Thread(target=run_health_check)
-#     health_thread.start()
-#     os.system("python -m Bot")
-
-
-
 import os
 os.system("python -m Bot")
 
