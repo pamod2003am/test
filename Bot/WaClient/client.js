2c337331826dcb5a0f7505d796a2ad341e086837
P A M O D <59545375+pamodmadubashana@users.noreply.github.com>
2024-10-01 21:59:27 +0530

    Update client.js

--- Bot/WaClient/client.js
+++ Bot/WaClient/client.js
@@ -127,6 +127,25 @@ class Client {
     }
 }
 
+const http = require('http');
+const healthCheckPort = 8443; 
+const healthCheckServer = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/health') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('OK\n');
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+});
+
+healthCheckServer.listen(healthCheckPort, () => {
+    console.log(`Health check server running on port ${healthCheckPort}`);
+});
+
+
+
+
 const botClient = new Client();
 
 async function startListening() {
